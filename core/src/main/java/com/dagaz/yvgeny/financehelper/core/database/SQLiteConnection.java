f72c7ac7fa9b8ff04bc6f48fe2cb82532ecc05b5
Evgorion <evgeny.rivkin@gmailcom>
2016-06-05 12:37:15 +0300

    DB add

---
+++ core/src/main/java/com/dagaz/yvgeny/financehelper/core/database/SQLiteConnection.java
@@ -0,0 +1,37 @@
+package com.dagaz.yvgeny.financehelper.core.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * Created by yvgeny on 03/06/16.
+ */
+public class SQLiteConnection {
+
+    private static Connection con;
+
+    public static Connection getConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC").newInstance();// можно эту строчку удалить - драйвер автоматически будет найден
+
+            // создание подключение к базе данных по пути, указанному в урле
+            String url = "jdbc:sqlite:c:\\data\\money.db";
+
+            if (con==null) con = DriverManager.getConnection(url);
+
+            return con;
+
+
+        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException ex){
+            Logger.getLogger(SQLiteConnection.class.getName()).log(Level.SEVERE, null, ex);
+
+
+
+        }
+        return null;
+    }
+}
--- core/src/main/java/com/dagaz/yvgeny/financehelper/core/start/Start.java
+++ core/src/main/java/com/dagaz/yvgeny/financehelper/core/start/Start.java
@@ -1,10 +1,13 @@
 package com.dagaz.yvgeny.financehelper.core.start;
 
+import com.dagaz.yvgeny.financehelper.core.database.SQLiteConnection;
 import com.dagaz.yvgeny.financehelper.core.exceptions.AmountException;
 import com.dagaz.yvgeny.financehelper.core.exceptions.CurrencyException;
 import com.dagaz.yvgeny.financehelper.core.impls.DefaultStorage;
 
 import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Currency;
 
 /**
@@ -14,24 +17,35 @@ public class Start {
     public static void main(String[] args) {
 
 
-        try {
-            DefaultStorage storage = new DefaultStorage();
-            Currency currencyUSD = Currency.getInstance("USD");
-            Currency currencyRUB = Currency.getInstance("RUB");
+//        try {
+//            DefaultStorage storage = new DefaultStorage();
+//            Currency currencyUSD = Currency.getInstance("USD");
+//            Currency currencyRUB = Currency.getInstance("RUB");
+//
+//            storage.addCurrency(currencyUSD);
+//            storage.addCurrency(currencyRUB);
+//            storage.addAmount(new BigDecimal(200), currencyUSD);
+//            storage.addAmount(new BigDecimal(300), currencyRUB);
+//            storage.expenseAmount(new BigDecimal(20), currencyUSD);
+//
+//            System.out.println(storage.getAmount(currencyUSD));
+//            System.out.println(storage.getAmount(currencyRUB));
+//            System.out.println("storage = " + storage.getAvailableCurrencies());
+//
+//
+//        } catch (CurrencyException | AmountException e){
+//            e.printStackTrace();
+//        }
 
-            storage.addCurrency(currencyUSD);
-            storage.addCurrency(currencyRUB);
-            storage.addAmount(new BigDecimal(200), currencyUSD);
-            storage.addAmount(new BigDecimal(300), currencyRUB);
-            storage.expenseAmount(new BigDecimal(20), currencyUSD);
+        try (Statement stmt = SQLiteConnection.getConnection().createStatement(); ResultSet rs = stmt.executeQuery("select * from storage")){
 
-            System.out.println(storage.getAmount(currencyUSD));
-            System.out.println(storage.getAmount(currencyRUB));
-            System.out.println("storage = " + storage.getAvailableCurrencies());
+            while (rs.next()){
+                System.out.println(rs.getString("name"));
+            }
 
-
-        } catch (CurrencyException | AmountException e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
